6961351ba7318baccbc313c46606940f1149e10b
baalintka <ballint3@gmail.com>
2024-04-10 20:44:12 +0200

    tesztelés listákra

--- teszt.js
+++ teszt.js
@@ -12,4 +12,28 @@ function  teszfuggveny() {
         "a várt eredmény nem stimmel"
         )
     }
+    {
+        const LISTA=["O","X","X","X","O"]
+        let vart="OXX@XO"
+        console.assert(
+
+            vart===vizszintes_ell(3,LISTA),
+        "%o",
+        `lista=${LISTA},meret=${3},lnko=${vart}`,
+        "a várt eredmény nem stimmel"
+        )
+
+    }
+    {
+        const LISTA=["O","X","X"]
+        let vart="OXX@"
+        console.assert(
+
+            vart===vizszintes_ell(3,LISTA),
+        "%o",
+        `lista=${LISTA},meret=${3},lnko=${vart}`,
+        "a várt eredmény nem stimmel"
+        )
+
+    }
 }
