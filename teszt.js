f9b2ddcc1c102d56689cb4d6382a6904d3d1f3a8
baalintka <ballint3@gmail.com>
2024-04-17 21:30:42 +0200

    .

--- teszt.js
+++ teszt.js
@@ -2,8 +2,8 @@ import { vizszintes_ell } from "./fuggvenyek.js";
 tesztfuggveny();
 export function  tesztfuggveny() {
     {
-        const LISTA=[]
-        let vart="hdfhgkd"
+        const LISTA=[         ]
+        let vart=""
         console.assert(
 
             vart===vizszintes_ell(3,LISTA),
@@ -13,20 +13,9 @@ export function  tesztfuggveny() {
         )
     }
     {
-        const LISTA=["O","X","X","X","O"]
-        let vart="OXX@XO"
-        console.assert(
-
-            vart===vizszintes_ell(3,LISTA),
-        "%o",
-        `lista=${LISTA},meret=${3},lnko=${vart}`,
-        "a várt eredmény nem stimmel"
-        )
-
-    }
-    {
-        const LISTA=["O","X","X"]
-        let vart="OXX@"
+        const LISTA=["O","X","X","X","O","O","O","O","O"]
+        let vart="OXX@XOO@OOO@"
+        console.log(vart)
         console.assert(
 
             vart===vizszintes_ell(3,LISTA),
@@ -37,8 +26,20 @@ export function  tesztfuggveny() {
 
     }
     {
-        const LISTA=["A","B","C"]
-        let vart="ABC@"
+        const LISTA=["A","B","C","D","E","F","G","H","I"]
+        let vart="ABC@DEF@GHI@"
+        console.assert(
+
+            vart===vizszintes_ell(3,LISTA),
+        "%o",
+        `lista=${LISTA},meret=${3},lnko=${vart}`,
+        "a várt eredmény nem stimmel"
+        )
+
+    }
+    {
+        const LISTA=[1,2,3,4,5,6,7,8,9]
+        let vart="123@456@789@"
         console.assert(
 
             vart===vizszintes_ell(3,LISTA),
